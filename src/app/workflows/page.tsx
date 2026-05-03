861aaa3e0b93d0881897866584717c6e6fd6d666
User <user@Users-iMac.modem>
2026-05-03 15:58:35 +1000

    Fix workflows page padding

--- src/app/workflows/page.tsx
+++ src/app/workflows/page.tsx
@@ -3,7 +3,7 @@ import { WorkflowsView } from '@/components/workflows/WorkflowsView'
 
 export default function WorkflowsPage() {
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex h-screen overflow-hidden p-7">
       <WorkflowsView />
     </div>
   )
